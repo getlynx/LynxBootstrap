0004d5e28a8e770907286e3c8f6f31d9d994e30e
Lynx Blockchain Development <38914109+getlynx@users.noreply.github.com>
2022-02-13 22:05:31 -0500

    Create backup.sh

---
+++ backup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+
+[ $EUID -ne 0 ] && echo "This script must be run from the root account. Exiting." && exit
+
+cd && currentDate=$(date +%F)
+
+echo "Backup process is running. This may take a while."
+
+tar -cf - --ignore-failed-read -C /home/lynx/.lynx/blocks . | gzip | split -b 125M - "$currentDate"-blocks.tar.gz.
+tar -cf - --ignore-failed-read -C /home/lynx/.lynx/chainstate . | gzip | split -b 125M - "$currentDate"-chainstate.tar.gz.
+
+sha256sum "$currentDate"-* > manifest.txt
