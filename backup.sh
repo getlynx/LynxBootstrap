c966f6375173b41104efa87b6bf1c3c3a3369e04
Lynx Blockchain Development <38914109+getlynx@users.noreply.github.com>
2022-02-22 22:03:29 -0500

    Testnet support

--- backup.sh
+++ backup.sh
@@ -3,6 +3,8 @@ PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 #
 # wget -qO - https://backup.getlynx.io/ | bash
 #
+# https://docs.getlynx.io/lynx-administration/snapshots
+#
 
 [ $EUID -ne 0 ] && echo "This script must be run from the root account. Exiting." && exit
 
@@ -12,7 +14,13 @@ cd && currentDate=$(date +%F)
 
 echo "Backup process is running. This may take a while."
 
-tar -cf - --ignore-failed-read -C /home/lynx/.lynx/blocks . | gzip | split -b 125M - "$currentDate"-blocks.tar.gz.
-tar -cf - --ignore-failed-read -C /home/lynx/.lynx/chainstate . | gzip | split -b 125M - "$currentDate"-chainstate.tar.gz.
+if [[ -d /home/lynx/.lynx/testnet4 ]] # Check if testnet or mainnet
+then
+	tar -cf - --ignore-failed-read -C /home/lynx/.lynx/testnet4/blocks . | gzip | split -b 125M - "$currentDate"-blocks.tar.gz.
+	tar -cf - --ignore-failed-read -C /home/lynx/.lynx/testnet4/chainstate . | gzip | split -b 125M - "$currentDate"-chainstate.tar.gz.
+else
+	tar -cf - --ignore-failed-read -C /home/lynx/.lynx/blocks . | gzip | split -b 125M - "$currentDate"-blocks.tar.gz.
+	tar -cf - --ignore-failed-read -C /home/lynx/.lynx/chainstate . | gzip | split -b 125M - "$currentDate"-chainstate.tar.gz.
+fi
 
 sha256sum "$currentDate"-* > manifest.txt
