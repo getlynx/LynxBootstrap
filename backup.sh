417351a7f45e3868c456f62491d8cb5ac9da87c0
Lynx Blockchain Development <38914109+getlynx@users.noreply.github.com>
2022-02-22 21:55:32 -0500

    custom hostname

--- backup.sh
+++ backup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 #
-# wget -qO - https://raw.githubusercontent.com/getlynx/LynxBootstrap/master/backup.sh | bash
+# wget -qO - https://backup.getlynx.io/ | bash
 #
 
 [ $EUID -ne 0 ] && echo "This script must be run from the root account. Exiting." && exit
