087041d35452d96f451e3cb34c220d92c14a6224
Lynx Blockchain Development <38914109+getlynx@users.noreply.github.com>
2022-05-16 00:46:43 -0400

    initial commit

---
+++ extract.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+#
+# wget -qO - https://extract.getlynx.io/
+#
+# [with tag argument]
+# wget -qO - https://extract.getlynx.io/ | bash -s v8.0-mainnet
+#
+# https://docs.getlynx.io/lynx-administration/snapshots
+#
+
+[ -z "$1" ] && tag="v8.0-mainnet" || tag="$1" # vX.0-mainnet is default.
+
+release="https://github.com/getlynx/LynxBootstrap/releases/download/$tag/" # Release version
+
+cd /home/lynx/.lynx || exit
+rm -rf manifest.txt # If previously run, purge old file
+wget -q "$release"manifest.txt # Pull down the manifest
+sed -i 's/  /:/' manifest.txt # Clean it up for easy usage
+while IFS= read -r line; do # Loop over each file and check the hashes match
+	hash=$(echo "$line" | cut -d ":" -f 1)
+	file=$(echo "$line" | cut -d ":" -f 2)
+	if [ ! -f "$file" ]; then # Only download the file if it doesn't exist locally
+		wget -q "$release$file"
+	fi
+	if [ "$(sha256sum "$file" | awk '{print $1}')" = "$hash" ]; then
+		echo "LynxCI: $file hash match $hash"
+	else # Hash doesn't match
+		rm -rf "$file"
+		wget -q "$release$file"
+		if [ "$(sha256sum "$file" | awk '{print $1}')" = "$hash" ]; then
+			echo "LynxCI: Sanity check - $file hash match $hash"
+		else
+			echo "LynxCI: Corrupt bootstrap file"
+			exit
+		fi
+	fi
+done < manifest.txt
+rm -rf manifest.txt
+
+cat ./*bootstrap.tar.gz.* | gunzip | (tar xf -)
+rm -rf ./*bootstrap.tar.gz*
