bddb38ba1c83ac3320ca3a3d462d677ae4d609ca
Lynx Blockchain Development <38914109+getlynx@users.noreply.github.com>
2022-07-06 21:59:55 -0400

    Testing

--- extract.sh
+++ extract.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 #
-# wget -qO - https://getlynx.io/extract.sh | bash
+# This script should be run as the Lynx user with regard to LynxCI. It assumes Lynx is already installed.
 #
-# [with tag argument]
-# wget -qO - https://getlynx.io/extract.sh | bash -s v8.0-mainnet
+# wget -O - https://raw.githubusercontent.com/getlynx/LynxBootstrap/master/extract.sh | bash
 #
 # https://docs.getlynx.io/lynx-administration/bootstraps
 #
 
-[ -z "$1" ] && tag="v8.0-mainnet" || tag="$1" # vX.0-mainnet is default.
+[ -z "$1" ] && tag="v9.0-mainnet" || tag="$1" # vX.0-mainnet is default.
 
 release="https://github.com/getlynx/LynxBootstrap/releases/download/$tag/" # Release version
 
